8a5e2cd19fd43b1e06a04ae10fd9d0df8c3f7698
pavi <mirceashark2007@yahoo.com>
2015-01-05 16:11:33 +0200

    Finished the guess method

--- bin/codebreaker.rb
+++ bin/codebreaker.rb
@@ -2,4 +2,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'codebreaker'
 game = Codebreaker::Game.new(STDOUT)
-game.start
+secret = ""
+(0..4).each do
+ secret << (1 + rand(6)).to_s 
+end
+game.start(secret)
+while guess = gets.chomp
+  game.guess(guess)
+end
